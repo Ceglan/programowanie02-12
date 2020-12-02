b8062ba754fb4aa62cdd9e3a3feb80ad1c6d7f03
Cianpadre <74541090+Cianpadre@users.noreply.github.com>
2020-12-02 09:06:50 +0100

    Update kart.cpp

--- kart.cpp
+++ kart.cpp
@@ -6,12 +6,10 @@ using namespace std;
 
 int main()
 {
-
 	random_device r;
-
 	default_random_engine e1(r());
-  uniform_int_distribution<int> uniform_dist(1, 6);
-  int los = uniform_dist(e1);
+	uniform_int_distribution<int> uniform_dist(1, 6);
+	int los = uniform_dist(e1);
 
 	return los;
 }
